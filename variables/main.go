d0c9637a1ecf4a0204d49c3dc7012e7d0165387a
Kindofabuzz <kindofabuzz@gmail.com>
2025-06-11 16:47:35 -0500

    initial

---
+++ variables/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+const prompt = "and press ENTER when ready."
+
+func main() {
+	var firstNumber = 2
+	var secondNumber = 5
+	var subtraction = 7
+	// var answer int
+
+	reader := bufio.NewReader(os.Stdin)
+
+	// display a welcome/instructions
+	fmt.Println("Guess the Number Game")
+	fmt.Println("---------------------")
+	fmt.Println("")
+
+	fmt.Println("Think of a number between 1 and 10", prompt)
+	reader.ReadString('\n')
+
+	// take them through the games
+	fmt.Println("Multiply your number by", firstNumber, prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Multiply the result by", secondNumber, prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Divide the result by the number you originally thought of", prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Now subtract", subtraction, prompt)
+	reader.ReadString('\n')
+
+	// give them the answer
+}
