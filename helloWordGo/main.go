a80da6ea8c410c7ff660a1eddae802d92b78dd57
Kindofabuzz <kindofabuzz@gmail.com>
2025-06-11 00:44:06 -0500

    initial

--- helloWordGo/main.go
+++ helloWordGo/main.go
@@ -1,9 +1,34 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"myapp/doctor"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hey now!!")
+	reader := bufio.NewReader(os.Stdin)
+
+	whatToSay := doctor.Intro()
+
+	fmt.Println(whatToSay)
+
+	for {
+		fmt.Print("-> ")
+		userInput, _ := reader.ReadString('\n')
+
+		userInput = strings.Replace(userInput, "\r\n", "", -1) // for Windows
+		userInput = strings.Replace(userInput, "\n", "", -1)   // For Mac/Linux
+
+		if userInput == "quit" {
+			break
+		} else {
+			fmt.Println(doctor.Response(userInput))
+
+		}
+
+	}
+
 }
