e0517a9f2e5b81e42821cff9afa1ccab482b510f
Kindofabuzz <kindofabuzz@gmail.com>
2025-06-12 19:51:37 -0500

    final exercise for scopes

--- scope/packageone/package.go
+++ scope/packageone/package.go
@@ -4,25 +4,6 @@ import "fmt"
 
 var PackageVar = 3
 
-func instructions() {
-	// variables
-	// declare a package level variable for the main
-	// package named myVar
-
-	// declare a block level variable for the main function
-	// called blockVar
-
-	// declare a package level variable in the packageone
-	// package named PackageVar
-
-	// create an exported function in packageone called PrintMe
-
-	// in the main function, print out the values of myVar,
-	// blockVar, and PackageVar on one line using the PrintMe
-	// function in packageone.
-
-}
-
 func PrintMe(i1, i2 int) {
 	fmt.Println(i1, i2, PackageVar)
 }
